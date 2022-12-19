2ee645b0158046b9ac8c8ceb143156cb32a332f0
sandymany <leticija.crnkovic@fer.hr>
2022-12-19 13:47:01 +0100

    lab 3

--- angular-flask/app.py
+++ angular-flask/app.py
@@ -6,6 +6,7 @@ from flask import Response
 import csv
 from database_service import DatabaseService
 from typing import Any
+import traceback
 
 
 # da bi aplikacija mogla biti servirana potrebno je:
@@ -15,8 +16,9 @@ from typing import Any
 # kopirati i index.html iz dist u templates od app.py
 # tako bi sve trebalo dobro raditi
 # moram jos isprobati jel budu assets tak radili isto
+# update: asseti rade, samo je potrebno dodati putanju u angular.json
 
-database_service = DatabaseService("insects.db")
+database_service = DatabaseService("insects1.db")
 
 app = Flask(__name__)
 # if app.debug:
@@ -29,6 +31,11 @@ white = ['http://127.0.0.1:4200']
 def hello_world():
     return render_template("index.html")
 
+@app.route('/spec.yaml')
+def yaml():
+  print("loadam")
+  return render_template("spec.yaml")
+
 @app.get('/data/<type>')
 def get_product(type):
   j = ""
@@ -37,54 +44,121 @@ def get_product(type):
       j = json.load(f)
     return json.dumps(j)
   elif type == "csv":
-    print("GETTING CSV DATA")
     c = ""
     with open("static/files/csv/insects.csv", "r") as f:
       c = f.read()
-    return json.dumps({"csv" : c})
+    response = Response(json.dumps({"csv": c}), 200, None, 'application/json')
+    # return json.dumps({"csv" : c})
+    return response
 
-@app.route('/download/<type>')
+@app.route('/download/<type>', methods = ['GET'])
 def download_file(type):
   params: dict[str, Any] = request.args.to_dict()
-  print("PARAMS ", params.keys())
   attribute = list(params.keys())[0]
   value = params[attribute]
 
-  print("GOT PARAMS: ", params)
   data = database_service.get_joined_where(attribute, value)
   # json format:
   if type == 'json':
-    print("PREPARING JSON")
     s = json.dumps({"data": data})
-    print("json: ", data)
     return Response(str.encode(s), mimetype='application/json', headers={'Content-Disposition':'attachment;filename=insects.json'})
   # csv format:
   elif type == 'csv':
-    print("PREPARING CSV")
-    print("make csv from: ", data)
-    rows = database_service.getCsvRowsFromJsonsList(data)
-    print("I got rows: ", rows)
+    rows = database_service.get_csv_rows_from_jsons_list(data)
     s = "\n".join(rows)
     response = Response(str.encode(s), mimetype='text/csv')
     response.headers['Content-Disposition'] = 'attachment; filename=insects.csv'
+    response.headers['Status'] = 200
     return response
   return (json.dumps({"data":"No given file type!"}))
- 
-@app.get('/insects')
+
+@app.route('/insects/<id>', methods = ['DELETE'])
+def delete_existing_insect(id):
+  r = database_service.delete_existing_insect(id)
+  response = Response(json.dumps({"data" : r}), mimetype='application/json')
+  response.status = 'error_message' in r.keys() and 404 or 200
+  return response
+
+# update postojeceg insekta
+# required request data: insect id (in url), data(in body) that has to be altered.
+@app.route('/insects/<id>', methods = ['PUT'])
+def update_existing_insect(id):
+  form: dict[str, Any] = request.form.to_dict()
+  r = database_service.update_existing_insect(id, form)
+  response = Response(json.dumps({"data" : r}), mimetype='application/json')
+  response.status = 'error_message' in r.keys() and 404 or 200
+  return r
+
+# treba vratiti novo stvoreni objekt s njegovim identifikatorom.
+# obavezna polja koja moraju doci u requestu: valid order_name, genus, specie.
+# ostala polja bi bilo dobro da ima, ali nisu krucijalna.
+@app.route('/insects', methods = ['POST'])
+def post_new_insect():
+  form: dict[str, Any] = request.form.to_dict()
+  try:
+    new_insect = database_service.insert_new_insect(form)
+    return json.dumps({"insect" : new_insect})
+
+  except:
+    traceback.print_exc()
+    response = Response(json.dumps({"error_message":"Request is invalid! Required in body: order_name, genus, specie."}))
+    response.status = 400
+    response.mimetype = 'application/json'
+    return response
+
+# speciesCount?low=0&high=0
+@app.route('/orders/speciesCount', methods = ['GET'])
+def get_orders_where_species_count():
+  params: dict[str, Any] = request.args.to_dict()
+  r = database_service.get_orders_by_species_count(params)
+  response = Response(json.dumps({"data": r}))
+  response.status = 200
+  response.mimetype = 'application/json'
+  return response
+
+@app.route('/orders/<id>/metamorphosis', methods = ['GET'])
+def get_metamorphosis_by_order_id(id):
+  # ako resurs ne postoji, vratiti 404 i poruku o greski.
+  r = database_service.get_metamorphosis_by_order_id(id)
+  response = Response(json.dumps({"data" : r}))
+  response.status = 'error_message' in r.keys() and 404 or 200
+  response.mimetype = 'application/json'
+  return response
+
+@app.route('/orders/<id>', methods = ['GET'])
+def get_order_by_id(id):
+  # ako resurs ne postoji, vratiti 404 i poruku.
+  r = database_service.get_order_by_id(id)
+  response = Response(json.dumps({"data" : r}))
+  response.status = 'error_message' in r.keys() and 404 or 200
+  response.mimetype = 'application/json'
+  return response
+
+@app.route('/insects/<id>', methods = ['GET'])
+def get_insect_by_id(id):
+  # ako resurs ne postoji, vratiti 404 i poruku o greski.
+  r = database_service.get_insect_by_id(id)
+  response = Response(json.dumps({"data" : r}))
+  response.status = 'error_message' in r.keys() and 404 or 200
+  response.mimetype = 'application/json'
+  return response
+
+@app.route('/insects', methods = ['GET'])
 def get_data_table():
   params: dict[str, Any] = request.args.to_dict()
   if len(list(params.keys())) == 0:
-    print("NEMA QUERY PARAMETARA! vracam sve!")
     data = database_service.get_all_joined_insects_and_orders()
-    print("got data: \n", data)
-    return json.dumps({"data":data})
-
+    response = Response(json.dumps({"data": data}))
+    response.status = 200
+    response.mimetype = 'application/json'
+    return response
+    
   attribute = list(params.keys())[0]
   value = params[attribute]
-  print("SEARCHING IN DATABASE FOR: ", attribute, " ", value)
   fetch = database_service.get_joined_where(attribute, value)
   for f in fetch:
     print(f)
-  return json.dumps({"data": fetch})
+  response = Response(json.dumps({"data": fetch}), 200, None, 'application/json')
+  return response
 
 app.run(port=8080, debug=True)
--- angular-flask/database_service.py
+++ angular-flask/database_service.py
@@ -50,7 +50,7 @@ class DatabaseService():
     for i, value in enumerate(row)) for row in cursor.fetchall()]
     return r
 
-  def getCsvRowsFromJsonsList(self, jsonsList):
+  def get_csv_rows_from_jsons_list(self, jsonsList):
     columnsInsect = self.getAllColumnsFromTable("insects") # dobivanje svih columna iz tablice insects
     columnsOrders = self.getAllColumnsFromTable("orders") # dobivanje svih columna iz tablice insects
     columns = []
@@ -71,3 +71,129 @@ class DatabaseService():
     cursor = self.connection.cursor()
     cursor.execute(query)
     return list(map(lambda x: x[0], cursor.description))
+  
+  # post new insect
+  # throws exception if required data is not given
+  def insert_new_insect(self, form):
+    cursor = self.connection.cursor()
+    order_name = form['order_name']
+    form.pop('order_name')
+    q = "SELECT * FROM orders WHERE order_name LIKE '%s'" %order_name
+    cursor.execute(q)
+    r = [dict((cursor.description[i][0], value) \
+    for i, value in enumerate(row)) for row in cursor.fetchall()]
+    order_id = r[0]['order_id']
+
+    columns = ','.join(list(form.keys()))
+    values = ','.join(list(f"'{form[x]}'" for x in list(form.keys())))
+    print("columns: ", columns)
+    print("values: ", values)
+    query = "INSERT INTO insects (order_id,%s) VALUES (%s,%s);"%(columns, order_id, values)
+    cursor.execute(query)
+    self.connection.commit()
+
+    # za vracanje objekta novog insekta:
+    select = f"SELECT * FROM insects WHERE insect_id = {cursor.lastrowid};"
+    cursor.execute(select)
+    r = [dict((cursor.description[i][0], value) \
+    for i, value in enumerate(row)) for row in cursor.fetchall()]
+    return r[0]
+  
+  def delete_existing_insect(self, id):
+    cursor = self.connection.cursor()
+    select = f"SELECT * FROM insects where insect_id={id}"
+    cursor.execute(select)
+    r = [dict((cursor.description[i][0], value) \
+    for i, value in enumerate(row)) for row in cursor.fetchall()]
+
+    if len(r) == 0:
+      return {"error_message" : "No insect with given id!"}
+
+    q = f"DELETE FROM insects WHERE insect_id = {id}"
+    cursor.execute(q)
+    self.connection.commit()
+    return {"message" : "Insect deleted successfully!"}
+  
+  def update_existing_insect(self, id, form):
+    cursor = self.connection.cursor()
+    select = f"SELECT * FROM insects WHERE insect_id = {id};"
+    cursor.execute(select)
+    r = [dict((cursor.description[i][0], value) \
+    for i, value in enumerate(row)) for row in cursor.fetchall()]
+    if len(r) == 0:
+      return {"error_message" : "No insect with given id!"}
+    
+    equals = ','.join(list(f"{x}='{form[x]}'" for x in list(form.keys())))
+    q = f"UPDATE insects SET {equals} WHERE insect_id={id};"
+    cursor.execute(q)
+    self.connection.commit()
+
+    select = f"SELECT * FROM insects WHERE insect_id = {id};"
+    cursor.execute(select)
+    r = [dict((cursor.description[i][0], value) \
+    for i, value in enumerate(row)) for row in cursor.fetchall()]
+    return {"insect" : r[0], "message" : "Updated insect data."}
+  
+  def get_insect_by_id(self, id):
+    cursor = self.connection.cursor()
+
+    if not self.exists("insects", id):
+      return {"error_message" : "No insect with requested id."}
+    
+    q = f"SELECT * FROM insects WHERE insect_id = {id}"
+    cursor.execute(q)
+    r = [dict((cursor.description[i][0], value) \
+    for i, value in enumerate(row)) for row in cursor.fetchall()]
+    return r[0]
+  
+  def get_order_by_id(self, id):
+    cursor = self.connection.cursor()
+
+  def get_orders_by_species_count(self, params):
+    cursor = self.connection.cursor()
+    if 'low' in list(params.keys()) and 'high' in list(params.keys()):
+      q = f"SELECT * FROM orders WHERE total_order_species BETWEEN {params['low']} AND {params['high']};"
+    elif 'low' in list(params.keys()):
+      q = f"SELECT * FROM orders WHERE total_order_species > {params['low']}"
+    elif 'high' in list(params.keys()):
+      q = f"SELECT * FROM orders WHERE total_order_species < {params['high']}"
+    else:
+      r = {"error_message" : "No parameters. Required parameters are: low and/or high."}
+    try:
+      cursor.execute(q)
+      r = [dict((cursor.description[i][0], value) \
+      for i, value in enumerate(row)) for row in cursor.fetchall()]
+      return r
+    except:
+      return {"error_message" : "No specified low or high."}
+  
+  def get_metamorphosis_by_order_id(self, id):
+    cursor = self.connection.cursor()
+    if not self.exists("orders", id):
+      return {"error_message" : "No order with given id."}
+    
+    q = f"SELECT metamorphosis FROM orders WHERE order_id = {id};"
+    cursor.execute(q)
+    r = [dict((cursor.description[i][0], value) \
+      for i, value in enumerate(row)) for row in cursor.fetchall()]
+    return r[0]
+  
+  def get_order_by_id(self, id):
+    cursor = self.connection.cursor()
+    if not self.exists("orders", id):
+      return {"error_message" : "No order with given id."}
+    q = f"SELECT * FROM orders WHERE order_id = {id};"
+    cursor.execute(q)
+    r = [dict((cursor.description[i][0], value) \
+      for i, value in enumerate(row)) for row in cursor.fetchall()]
+    return r[0]
+
+  def exists(self, table, id):
+    cursor = self.connection.cursor()
+    select = f"SELECT * FROM orders WHERE order_id = {id};"
+    cursor.execute(select)
+    r = [dict((cursor.description[i][0], value) \
+    for i, value in enumerate(row)) for row in cursor.fetchall()]
+    if len(r) == 0:
+      return False
+    return True
